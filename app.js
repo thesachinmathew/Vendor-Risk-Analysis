f4c933a37434f9803ad834f4626c169e8bbbf912
Sachin Mathew <167275639+thesachinmathew@users.noreply.github.com>
2025-11-05 14:15:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect } from 'react';
 import { AlertCircle, Shield, Upload, LogOut, BarChart3, Plus, Eye, Download, ChevronLeft, ChevronRight } from 'lucide-react';
 
@@ -859,6 +860,7 @@ const AppContent = ({ currentPage, setCurrentPage, selectedVRM, setSelectedVRM }
   return renderPage();
 
 }
+
 
 
 
